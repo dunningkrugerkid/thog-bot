1a446617e8d182f8ef3048d9e9f0eb4b69da8217
Amélie <93049096+tickjesus@users.noreply.github.com>
2021-12-16 16:03:21 -0330

    first pass with a boolean test and some text reading

---
+++ thogbot.py
@@ -0,0 +1,49 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GUILD = os.getenv('DISCORD_GUILD')
+TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f"{client.user} don't caare\n"
+        f"{guild.name} is pretty cool.\n"
+    )
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'List of smelly people:\n - {members}')
+
+@client.event
+async def on_message(message):
+    channel = message.channel
+    if message.content.startswith('thogDecide'):
+        def decideFate():
+            import random
+            choice = random.randint(0,1)
+            return choice
+            
+        await channel.send("thog decide your fate")
+        await channel.send("thog decided this...")
+
+        choice = decideFate()
+  
+        if choice: # rolled a 0
+            await channel.send("thog pine for violence. where tob? we have job to do.")
+        else: # rolled a 1
+            await channel.send("thog dont caare about killing you")
+    elif message.content.startswith('does thog care'):
+        await channel.send("thog dont caare")
+
+        
+
+    
+    
+client.run(TOKEN)
